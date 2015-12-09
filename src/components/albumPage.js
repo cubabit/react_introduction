bb35eb1aa62f1e5a38316e45ff8704b2fce0c93e
Pete Smith <pete@cubabit.net>
2015-12-09 17:05:08 +0000

    Clean up design and code

--- src/components/albumPage.js
+++ src/components/albumPage.js
@@ -4,24 +4,36 @@ var React = require('react');
 var AlbumList = require('./albumList.js');
 
 module.exports = React.createClass({
-  onSearchChange: function(event) {
+  handleFilterChange: function(event) {
       var updatedList = this.props.albums.filter(function(album) {
         return album.title.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
       });
 
       this.setState({ albums: updatedList });
   },
+
   componentWillMount: function() {
     this.setState({albums: this.props.albums});
   },
+
   render: function() {
-    return <div className="container">
-      <div className="row">
-        <div className="col-sm-10 col-sm-offset-1 form-group">
-          <input type="text" placeholder="Search" className="form-control" onChange={this.onSearchChange} />
+    return (
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center">Classic 80's albums</h1>
         </div>
+        <div className="row">
+          <div className="col-sm-10 col-sm-offset-1 form-group">
+            <input
+              type="text"
+              placeholder="Filter"
+              className="form-control"
+              onChange={this.handleFilterChange}
+            />
+          </div>
+        </div>
+        <AlbumList albums={this.state.albums}/>
       </div>
-      <AlbumList albums={this.state.albums}/>
-    </div>;
+    );
   }
 });
