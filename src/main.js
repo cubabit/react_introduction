348b0cb50b3a79430f6e3cc95ce08e0e4728ff25
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-12-17 18:08:05 +0000

    adding strikethrough with parent in control

--- src/main.js
+++ src/main.js
@@ -6,43 +6,49 @@ var ReactDOM = require('react-dom');
 var itemList = [
   {label: 'Get shopping', isCompleted: false},
   {label: 'Put out rubbish', isCompleted: false},
-  {label: 'Watch Breaking Bad', isCompleted: true}
+  {label: 'Watch Breaking Bad', isCompleted: false}
 ];
 
 
-var ListItem = React.createClass({
-  componentWillMount: function () {
-    this.setState({
-      isCompleted: this.props.isCompleted
-    });
-  },
-  handleCheckboxClick: function (event) {
-    this.setState({isCompleted: !this.state.isCompleted});
-  },
-
+var Item = React.createClass({
   render: function () {
-    var label;
-    if (this.state.isCompleted) {
-      label = <s>{this.props.label}</s>
+    var labelTag;
+    if (this.props.isCompleted) {
+      labelTag = <s>{this.props.label}</s>;
     } else {
-      label = <span>{this.props.label}</span>
+      labelTag = <span>{this.props.label}</span>;
     } 
 
     return (
       <div>
-        <input type="checkbox" checked={this.state.isCompleted} onClick={this.handleCheckboxClick} /> {label}
+        <input type="checkbox" checked={this.props.isCompleted} onClick={this.props.onClick} /> {labelTag}
       </div>
     );
   }
 });
 
 var List = React.createClass({
+  getInitialState: function() {
+    return {items: this.props.list};
+  },
+
+  handleClick: function(index) {
+    var items = this.state.items;
+    items[index].isCompleted = !items[index].isCompleted;
+    this.setState({ items: items });
+  },
+
   listElements: function () {
-    return this.props.list.map(function (elem, index) {
-      return (
-        <li key={index}><ListItem label={elem.label} isCompleted={elem.isCompleted} /></li>
-      );
-    });
+    return (
+      <div>
+      {this.state.items.map(function (elem, index) {
+        var boundClick = this.handleClick.bind(this, index);
+        return (
+          <li key={index}><Item label={elem.label} isCompleted={elem.isCompleted} onClick={boundClick} /></li>
+        );
+      }, this)}
+      </div>
+    );
   },
 
   render: function () {
